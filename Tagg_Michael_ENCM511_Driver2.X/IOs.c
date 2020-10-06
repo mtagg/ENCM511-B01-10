618577a1931fc0ccc63d51f9018c91b8ebe3bdb0
mtagg <mmmtagg@gmail.com>
2020-10-05 19:12:59 -0600

    Fully Working Code - final revision
    Final revisions and modification involving setting all registers to digital IO  as per rushi's emails.
    
    Aside from format and potential comments, this code is version final.

--- Tagg_Michael_ENCM511_Driver2.X/IOs.c
+++ Tagg_Michael_ENCM511_Driver2.X/IOs.c
@@ -7,15 +7,14 @@ void delay_s(int t){
     return;
 }
 void IOinit(void){
+    AD1PCFG = 0xFFFF; // Turn all analog pins as digital 
     TRISBbits.TRISB4 = 1;      // set RB4 to digi input
-   // TRISAbits.TRISA2 = 1;      // set RA2 to digi input
-    TRISBbits.TRISB9 = 1;
+    TRISAbits.TRISA2 = 1;      // set RA2 to digi input
     TRISAbits.TRISA4 = 1;      // set RA4 to digi input
     TRISBbits.TRISB8 = 0;      // set RB8 to digi output for LED
     CNPU1bits.CN0PUE  = 1;       // pull up resistor for RA4/CN0
     CNPU1bits.CN1PUE  = 1;       // pull up resistor for RB4/CN1
-    CNPU2bits.CN21PUE = 1;
-	//CNPU2bits.CN30PUE = 1;       // pull up resistor for RA2/CN30
+	CNPU2bits.CN30PUE = 1;       // pull up resistor for RA2/CN30
 	return;
 }
 
@@ -26,7 +25,7 @@ void CheckPushButtons(void){
             delay_s(1);                     // 1 second delay 
         	LATBbits.LATB8 = 0;             // LED output off once again
 	} else
-	if (PORTBbits.RB9 == 0){                // RA2 button switched to ground
+	if (PORTAbits.RA2 == 0){                // RA2 button switched to ground
         	LATBbits.LATB8 = 1;             // Turns on led connected to RB8
         	delay_s(2);                     // 2 second delay
         	LATBbits.LATB8 = 0;             // turns off led connected to RB8
@@ -35,10 +34,10 @@ void CheckPushButtons(void){
         	LATBbits.LATB8 = 1;             // Turns on led connected to RB8
         	delay_s(3);                     // 3 second delay
         	LATBbits.LATB8 = 0;             // turns off led connected to RB8
-	} /*else
+	} else
 	if ((PORTAbits.RA2 + PORTAbits.RA4 + PORTBbits.RB4) < 2){ //2 or 3 buttons pressed 
         	LATBbits.LATB8 = 1;                                //Turns on led connected to RB8
-	}*/
+	}
     return;
 }
 
