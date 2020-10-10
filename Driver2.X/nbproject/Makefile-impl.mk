7c23b448c69340378ba63fb42f892bf757a71abd
mtagg <mmmtagg@gmail.com>
2020-10-10 13:58:11 -0600

    Version used for video demonstration

--- Driver2.X/nbproject/Makefile-impl.mk
+++ Driver2.X/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=Tagg_Michael_ENCM511_Driver2.X
+PROJECTNAME=Driver2.X
 
 # Active Configuration
 DEFAULTCONF=default
--- Driver2.X/nbproject/Makefile-variables.mk
+++ Driver2.X/nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=Tagg_Michael_ENCM511_Driver2.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/Tagg_Michael_ENCM511_Driver2.X.production.hex
+CND_ARTIFACT_NAME_default=Driver2.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Driver2.X.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=taggmichaelencm511driver2.x.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/taggmichaelencm511driver2.x.tar
+CND_PACKAGE_NAME_default=driver2.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/driver2.x.tar
